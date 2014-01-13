c99d87d86ce4438bc3d4d5889a6c4c6e7fe45c3a
Bjoern Ahlfeld <b.ahlfeld@gmail.com>
2014-01-13 19:16:27 +0100

    Added custom ApplicationConfiguration

---
+++ Android/src/LiarApplication.java
@@ -0,0 +1,18 @@
+import android.app.Application;
+import android.bluetooth.BluetoothDevice;
+
+
+public class LiarApplication extends Application {
+
+	private BluetoothDevice mBtDevice;
+	
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		
+	}
+	
+	public synchronized BluetoothDevice getBluetoothDevice() {
+		return mBtDevice;
+	}
+}
