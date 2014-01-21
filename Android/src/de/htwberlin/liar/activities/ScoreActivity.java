2bab3c7f991e928da697e0cb4d6e52f8d1bd6e6a
Bjoern Ahlfeld <b.ahlfeld@gmail.com>
2014-01-21 17:36:12 +0100

    Added cursorloader to scoreactivity

--- Android/src/de/htwberlin/liar/activities/ScoreActivity.java
+++ Android/src/de/htwberlin/liar/activities/ScoreActivity.java
@@ -1,16 +1,37 @@
 package de.htwberlin.liar.activities;
 
 
-import android.app.Activity;
+import android.app.ListActivity;
+import android.app.LoaderManager.LoaderCallbacks;
+import android.content.Loader;
+import android.database.Cursor;
 import android.os.Bundle;
 import de.htwberlin.liar.R;
 
-public class ScoreActivity extends Activity {
+public class ScoreActivity extends ListActivity implements LoaderCallbacks<Cursor> {
 
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_score);
+	}
+
+	@Override
+	public Loader<Cursor> onCreateLoader(int arg0, Bundle arg1) {
+		// TODO return a new cursorloader...
+		return null;
+	}
+
+	@Override
+	public void onLoadFinished(Loader<Cursor> arg0, Cursor arg1) {
+		// swap cursor for adapter...
+		
+	}
+
+	@Override
+	public void onLoaderReset(Loader<Cursor> arg0) {
+		// swap cursor in adapter to null...
+		
 	}
 }
